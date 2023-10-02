ebe7c0a2ff74d6b0ff5db3e1da1dfd0244de8ff7
321999 <kishorejha321999@gmail.com>
2023-10-03 01:16:17 +0530

    to run the python file which is the part of the django project we use djnago shell

--- home/models.py
+++ home/models.py
@@ -2,16 +2,25 @@ from django.db import models
 
 # Create your models here.
 # each field  is specified as a class attribute and each attribute is the tables column 
-class Student(models.Model):
+# class Student(models.Model):
    
-    name=models.CharField(max_length=30)
-    roll=models.IntegerField()
-    email=models.EmailField()
-    marks=models.IntegerField()
-    address=models.CharField(max_length=100)
-    image=models.ImageField(null=True)
-    file=models.FileField()
-
+   
+#     name=models.CharField(max_length=30)
+#     roll=models.IntegerField()
+#     email=models.EmailField()
+#     marks=models.IntegerField()
+#     address=models.CharField(max_length=100)
+   
 # creating the prouduct schema 
-class Product(models.Model):
-    pass
+# class Product(models.Model):
+#     name=models.CharField(max_length=30)
+#     email=models.EmailField(null=True,blank=True)
+#     star=models.IntegerField(null=True,blank=True)
+#     loc=models.CharField(max_length=100)
+
+
+class User(models.Model):
+    name=models.CharField(max_length=50)
+    email=models.EmailField(null=True,blank=True)
+    age=models.IntegerField()
+    address=models.TextField(null=True,blank=True)
