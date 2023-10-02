5ed197db82b2c4b36caee19cd2c60848a6becbce
321999 <kishorejha321999@gmail.com>
2023-10-02 19:32:25 +0530

    technique of models and migration,migrate

--- home/models.py
+++ home/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+# each field  is specified as a class attribute and each attribute is the tables column 
+class Student(models.Model):
+   
+    name=models.CharField(max_length=30)
+    roll=models.IntegerField()
+    email=models.EmailField()
+    marks=models.IntegerField()
+    address=models.CharField(max_length=100)
+    image=models.ImageField(null=True)
+    file=models.FileField()
+
+# creating the prouduct schema 
+class Product(models.Model):
+    pass
