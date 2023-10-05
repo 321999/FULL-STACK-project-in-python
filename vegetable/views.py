37f337439dacea84e688bcad4311a22f7cb841f3
321999 <kishorejha321999@gmail.com>
2023-10-05 23:54:23 +0530

    how to send the image from frontend to backend and store it in dbs

--- vegetable/views.py
+++ vegetable/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpRequest
+from .models import *
 # Create your views here.
 def recipe(request):
     if request.method=="POST":
@@ -10,9 +11,16 @@ def recipe(request):
         reciptImage=request.FILES.get('iamge')
 
         recipe_name=data['name']
+        # we are fetching data like this becaus we know that data is in object form 
         recipe_description=data.get('ingredient')
+        reveiw_star=data.get('review')
         # print(f"recipe name is {data.get('name')}")
         print(f"reciept name is {recipe_name}\n and the description is {recipe_description} \n image of reciept is:{reciptImage}")
-
+        Recipe.objects.create(
+            name=recipe_name,
+            ingredient=recipe_description,
+            review=reveiw_star,
+            iamge=reciptImage,
+        )
 
     return render(request,"recipe.html",context={"title":"recipe"})
